03fc35ad013cb6287e0d5774a92b1d9328241552
Prerna Jaiswal <prernakkjaiswal@gmail.com>
2026-04-19 11:36:39 +0530

    Update import path for utility function

--- Input.tsx
+++ Input.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cn } from '@/src/lib/utils';
+import { cn } from '@/utils';
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
