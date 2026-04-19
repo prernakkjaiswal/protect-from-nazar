b05c38d8fe0ae0cbfa7e849bf0992252ce0364ce
Prerna Jaiswal <prernakkjaiswal@gmail.com>
2026-04-19 11:34:53 +0530

    Update import path for utility functions

--- Button.tsx
+++ Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cn } from '@/src/lib/utils';
+import { cn } from '@/utils';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 const buttonVariants = cva(
